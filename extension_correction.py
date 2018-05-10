derekmorr/Shannon

commit d936ffdac1f24fbd851d05eb8f98d4b85812655a

make par_load() match load_kmers()
par_load() was using different arguments to split(). Also make it use named local variables and a defaultdict, to match the structure of load_kmers().

extension_correction.py
@@ -163,17 +163,20 @@ def argmax(lst, key):
     return best
 
 
-def par_load(lines, ds, polyA_del, out_q):
+def par_load(lines, double_stranded, polyA_del, out_q):
+    """Loads the list of K-mers and copycounts and determines K.
+    Returns (kmers, K) in out_q.
+    """
     reverse_complement = lambda x: ''.join([{'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}[B] for B in x][::-1])
-    d = {}
+    d = defaultdict(lambda: 0)
     for (i, line) in enumerate(lines):
-        line = line.strip().split(None, 1)
-        if polyA_del and lowComplexity(line[0]):
+        kmer, weight = line.strip().split()
+        if polyA_del and lowComplexity(kmer):
             continue
-        d[line[0]] = d.get(line[0], 0) + float(line[1])
-        if ds:
-            rc = reverse_complement(line[0])
-            d[rc] = d.get(rc, 0) + float(line[1])
+        d[kmer] += float(weight)
+        if double_stranded:
+            rc = reverse_complement(kmer)
+            d[rc] += float(weight)
     out_q.put(d)
 
 

commit 0a82ca3e0f2d6f38efe1ce723d1f3fbbac33e146

partially revert d936ffdac1f24fbd851d05eb8f98d4b85812655a
Commit d936ffdac1f24fbd851d05eb8f98d4b85812655a introduced a bug. par_load() was switched to use a defaultdict, which isn’t pickle-able. The dictionary needs to be picke-able because it’s put into a queue for shared-memory.

extension_correction.py
@@ -163,15 +163,18 @@ def par_load(lines, double_stranded, polyA_del, out_q):
     """
     reverse_complement = \
         lambda x: ''.join([{'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}[B] for B in x][::-1])
-    d = defaultdict(lambda: 0)
+
+    # don't use a defaultdict here because it's not pickle-able.
+    # "d" has to be pickle-able because it's put into out_q.
+    d = {}
     for (i, line) in enumerate(lines):
         kmer, weight = line.strip().split()
         if polyA_del and lowComplexity(kmer):
             continue
-        d[kmer] += float(weight)
+        d[kmer] = d.get(kmer, 0) + float(weight)
         if double_stranded:
             rc = reverse_complement(kmer)
-            d[rc] += float(weight)
+            d[rc] = d.get(rc, 0) + float(weight)
     out_q.put(d)
 
 