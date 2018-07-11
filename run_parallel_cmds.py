c21b45268f5c9a17f05b41eac52e648de1518864
Derek Morr <derekmorr@psu.edu>
2018-07-11 14:14:13 -0400

    fix indentation to python standard

--- run_parallel_cmds.py
+++ run_parallel_cmds.py
@@ -5,12 +5,12 @@ import run_MB_SF_fn
 def run_process(cmd):
     os.system(cmd)
 
-def run_cmds(cmds,noJobs):
+def run_cmds(cmds, num_jobs):
 	#cmds is a tuple of strings
-	#noJobs is a an integer with the no of Jobs to run in parallel
-	p = multiprocessing.Pool(noJobs)
-	p.map(run_process,cmds)
+	#num_jobs is a an integer with the no of Jobs to run in parallel
+    p = multiprocessing.Pool(num_jobs)
+    p.map(run_process, cmds)
 
 def run_MB_SF(cmds):
-	p = multiprocessing.Pool(noJobs)
-	p.map(run_MB_SF_fn.run_MB_SF_fn,cmds)
+    p = multiprocessing.Pool(num_jobs)
+    p.map(run_MB_SF_fn.run_MB_SF_fn, cmds)
