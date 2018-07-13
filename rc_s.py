72009b375aa22d1dafc9d325e30a9202df3edfd3
Derek Morr <derekmorr@psu.edu>
2018-07-13 13:51:06 -0400

    remove useless conditional

--- rc_s.py
+++ rc_s.py
@@ -5,15 +5,14 @@ reverse_complement = lambda x: ''.join([D[B] for B in x][::-1])
 
 def reverse_complement_serial(infile, outfile):
     reads = []
-    if 1: #with open(outfile,'w') as of:
-        for line in open(infile):
-            if not line.strip():
-                continue
-            fields = line.strip().split()
-            if fields[0][0] == '>':
-                reads.append(line.strip())
-            else:
-                reads.append(reverse_complement(fields[0]))
+    for line in open(infile):
+        if not line.strip():
+            continue
+        fields = line.strip().split()
+        if fields[0][0] == '>':
+            reads.append(line.strip())
+        else:
+            reads.append(reverse_complement(fields[0]))
     with open(outfile, 'w') as of:
         of.write('\n'.join(reads) + '\n')
 
