be3cae5a6910e9ee4538252e4354106481573f75
Derek Morr <derekmorr@psu.edu>
2018-07-13 15:34:29 -0400

    stream the results to output, don't the entire file in memory

--- rc_s.py
+++ rc_s.py
@@ -4,18 +4,17 @@ D = {'A':'T', 'C':'G', 'G':'C', 'T':'A', 'N':'N'}
 reverse_complement = lambda x: ''.join([D[B] for B in x][::-1])
 
 def reverse_complement_serial(infile, outfile):
-    reads = []
-    for line in open(infile):
-        if not line.strip():
-            continue
-        fields = line.strip().split()
-        if fields[0][0] == '>':
-            reads.append(line.strip())
-        else:
-            reads.append(reverse_complement(fields[0]))
     with open(outfile, 'w') as of:
-        of.write('\n'.join(reads) + '\n')
-
+        for line in open(infile):
+            if not line.strip():
+                continue
+            fields = line.strip().split()
+            if fields[0][0] == '>':
+                of.write(line.strip())
+                of.write('\n')
+            else:
+                of.write(reverse_complement(fields[0]))
+                of.write('\n')
 
 def main():
     if len(sys.argv) == 1:
